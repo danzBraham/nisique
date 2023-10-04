9a72b6c6e51e4dcdff4515b972d9bbc8d61e4fa2
Zidan Abraham <zidangit07@gmail.com>
2023-10-04 11:03:15 +0800

    feat(nav): implement the Nav component and add constants file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,29 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className="padding-x absolute z-10 w-full py-8">
+      <nav className="max-container flex items-center justify-between">
+        <img src={headerLogo} alt="Logo" width={130} />
+        <ul className="flex flex-1 items-center justify-center gap-16 max-lg:hidden">
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className="text-montserrat text-slate-gray text-lg leading-normal"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="lg:hidden">
+          <img src={hamburger} alt="hamburger" width={30} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 export default Nav;
